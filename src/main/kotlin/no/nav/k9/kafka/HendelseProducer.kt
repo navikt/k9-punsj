0558f77a202e43ad9bc4062b0edab2160635caf9
olovholm <ola.lovholm@nav.no>
2020-02-27 00:09:19 +0100

    Endrer til mer Spring Boot-aktig implementasjon.

--- src/main/kotlin/no/nav/k9/kafka/HendelseProducer.kt
+++ src/main/kotlin/no/nav/k9/kafka/HendelseProducer.kt
@@ -1,34 +1,94 @@
 package no.nav.k9.kafka
 
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.KafkaException
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.SendResult
+import org.springframework.util.concurrent.ListenableFuture
+import org.springframework.util.concurrent.ListenableFutureCallback
+import java.util.*
 
-@Service
-class HendelseProducer @Autowired constructor(
-        private val kafkaPropertiesUtil: KafkaPropertiesUtil
-) {
 
-    private val producer = KafkaProducer<String, String>(kafkaPropertiesUtil.opprettProperties())
+@Configuration
+class HendelseProducer {
 
+    @Value("\${kafka.bootstrap.server}")
+    val bootstrapServers = ""
+    @Value("\${kafka.clientId}")
+    val clientId = ""
+    @Value("\${systembruker.username}")
+    val username = ""
+    @Value("\${systembruker.password}")
+    val password = ""
+    @Value("\${javax.net.ssl.trustStore}")
+    val trustStorePath = ""
+    @Value("\${javax.net.ssl.trustStorePassword}")
+    val trustStorePassword = ""
 
     private companion object {
         private val logger: Logger = LoggerFactory.getLogger(HendelseProducer::class.java)
     }
 
-    fun sendKafkaMessage(topic: String, melding: String) {
-        try {
-            producer.send(ProducerRecord(topic, melding)).get()
-            logger.info("Søknad lagt på kafka-topic $topic")
-        } catch (kafkaException: KafkaException) {
-            logger.warn("Innsending av melding feilet: ${kafkaException.message}")
-            producer.abortTransaction()
+    @Bean
+    fun producerFactory(): ProducerFactory<Int?, String?>? {
+        return DefaultKafkaProducerFactory(producerConfigs()!!)
+    }
+
+    @Bean
+    fun producerConfigs(): Map<String, Any>? {
+        val props: MutableMap<String, Any> = HashMap()
+        props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+        props.put(CommonClientConfigs.CLIENT_ID_CONFIG, clientId)
+        props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+
+        setSecurity(username, props)
+        setUsernameAndPassword(username, password, props)
+
+        return props
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<Int?, String?>? {
+        return KafkaTemplate(producerFactory()!!)
+    }
+
+    private fun setSecurity(username: String?, properties: MutableMap<String,Any>) {
+        if (username != null && !username.isEmpty()) {
+            properties["security.protocol"] = "SASL_SSL"
+            properties["sasl.mechanism"] = "PLAIN"
+            properties[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = trustStorePath
+            properties[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = trustStorePassword
         }
     }
 
+    private fun setUsernameAndPassword(username: String?, password: String?, properties: MutableMap<String, Any>) {
+        if (username != null && !username.isEmpty()
+                && password != null && !password.isEmpty()) {
+            val jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";"
+            val jaasCfg = String.format(jaasTemplate, username, password)
+            properties["sasl.jaas.config"] = jaasCfg
+        }
+    }
 
+    fun sendToKafka(topicName: String, message: String) {
+        val future: ListenableFuture<SendResult<Int?, String?>> = kafkaTemplate()!!.send(topicName,message)
+        future.addCallback(object : ListenableFutureCallback<SendResult<Int?, String?>?> {
+            override fun onSuccess(result: SendResult<Int?, String?>?) {
+                logger.info("Melding sendt på Kafka-topic: $topicName")
+            }
+            override fun onFailure(ex: Throwable) {
+                logger.warn("Kunne ikke legge søknad på Kafka-topic $topicName : ${ex.message}" )
+            }
+        })
+    }
 }
--- src/main/kotlin/no/nav/k9/pleiepengersyktbarn/soknad/PleiepengerSyktBarnSoknadService.kt
+++ src/main/kotlin/no/nav/k9/pleiepengersyktbarn/soknad/PleiepengerSyktBarnSoknadService.kt
@@ -10,15 +10,13 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class PleiepengerSyktBarnSoknadService {
-
-
-    @Autowired
-    lateinit var hendelseProducer: HendelseProducer
+class PleiepengerSyktBarnSoknadService @Autowired constructor(
+        var hendelseProducer: HendelseProducer
+) {
 
     private companion object {
         private val logger: Logger = LoggerFactory.getLogger(PleiepengerSyktBarnSoknadService::class.java)
-        const val PLEIEPENGER_SYKT_BARN_TOPIC = "privat-omsorgspengesoknad-journalfort"
+        const val PLEIEPENGER_SYKT_BARN_TOPIC = "punsjet-soknad-pleiepenger-barn"
     }
 
     internal suspend fun sendSøknad(
@@ -28,8 +26,7 @@ class PleiepengerSyktBarnSoknadService {
         val dummyJsonMessage = "{\"versjon\":\"1.0.0\",\"søknadId\":\"1\",\"mottattDato\":\"2019-10-20T07:15:36.124Z\",\"språk\":\"nb\",\"søker\":{\"norskIdentitetsnummer\":\"12345678901\"},\"perioder\":{\"2018-12-30/2019-10-20\":{}},\"barn\":{\"fødselsdato\":null,\"norskIdentitetsnummer\":\"12345678902\"},\"bosteder\":{\"perioder\":{\"2022-12-30/2023-10-10\":{\"land\":\"POL\"}}},\"utenlandsopphold\":{\"perioder\":{\"2018-12-30/2019-10-10\":{\"land\":\"SWE\",\"årsak\":\"barnetInnlagtIHelseinstitusjonForNorskOffentligRegning\"},\"2018-10-10/2018-10-30\":{\"land\":\"NOR\",\"årsak\":null},\"2021-10-10/2050-01-05\":{\"land\":\"DEN\",\"årsak\":\"barnetInnlagtIHelseinstitusjonDekketEtterAvtaleMedEtAnnetLandOmTrygd\"}}},\"beredskap\":{\"perioder\":{\"2018-10-10/2018-12-29\":{\"tilleggsinformasjon\":\"Noe tilleggsinformasjon. Lorem ipsum æÆøØåÅ.\"},\"2019-01-01/2019-01-30\":{\"tilleggsinformasjon\":\"Noe tilleggsinformasjon. Lorem ipsum æÆøØåÅ.\"}}},\"nattevåk\":{\"perioder\":{\"2018-10-10/2018-12-29\":{\"tilleggsinformasjon\":\"Noe tilleggsinformasjon. Lorem ipsum æÆøØåÅ.\"},\"2019-01-01/2019-01-30\":{\"tilleggsinformasjon\":\"Noe tilleggsinformasjon. Lorem ipsum æÆøØåÅ.\"}}},\"tilsynsordning\":{\"iTilsynsordning\":\"ja\",\"opphold\":{\"2019-01-01/2019-01-01\":{\"lengde\":\"PT7H30M\"},\"2020-01-02/2020-01-02\":{\"lengde\":\"PT7H25M\"},\"2020-01-03/2020-01-09\":{\"lengde\":\"PT168H\"}}},\"arbeid\":{\"arbeidstaker\":[{\"organisasjonsnummer\":\"999999999\",\"norskIdentitetsnummer\":null,\"perioder\":{\"2018-10-10/2018-12-29\":{\"skalJobbeProsent\":50.25}}},{\"organisasjonsnummer\":null,\"norskIdentitetsnummer\":\"29099012345\",\"perioder\":{\"2018-11-10/2018-12-29\":{\"skalJobbeProsent\":20}}}],\"selvstendigNæringsdrivende\":[{\"perioder\":{\"2018-11-11/2018-11-30\":{}}}],\"frilanser\":[{\"perioder\":{\"2019-10-10/2019-12-29\":{}}}]},\"lovbestemtFerie\":{\"perioder\":{\"2018-11-10/2018-12-29\":{}}}}\n"
 
         //TODO: Bytte ut dummy-json med reell punchet søknad
-        hendelseProducer.sendKafkaMessage(PLEIEPENGER_SYKT_BARN_TOPIC, dummyJsonMessage)
-        // TODO: Legge på en kafka-topic k9-fordel håndterer.
+        hendelseProducer.sendToKafka(PLEIEPENGER_SYKT_BARN_TOPIC, dummyJsonMessage)
     }
 
 
