47b9345e628ef3194dbdb25eee172c2651da054a
olovholm <ola.lovholm@nav.no>
2020-02-26 12:41:16 +0100

    Endrer generering av KafkaProducer samt innskyting av KafkaPropertyUtils

--- src/main/kotlin/no/nav/k9/kafka/HendelseProducer.kt
+++ src/main/kotlin/no/nav/k9/kafka/HendelseProducer.kt
@@ -9,28 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class HendelseProducer {
+class HendelseProducer @Autowired constructor(
+        private val kafkaPropertiesUtil: KafkaPropertiesUtil
+) {
 
-    @Autowired
-    lateinit var kafkaPropertiesUtil: KafkaPropertiesUtil
+    private val producer = KafkaProducer<String, String>(kafkaPropertiesUtil.opprettProperties())
 
 
     private companion object {
         private val logger: Logger = LoggerFactory.getLogger(HendelseProducer::class.java)
     }
 
-    //TODO: Ikke instansiere producer
     fun sendKafkaMessage(topic: String, melding: String) {
-        val producer = KafkaProducer<String, String>(kafkaPropertiesUtil.opprettProperties())
         try {
             producer.send(ProducerRecord(topic, melding)).get()
             logger.info("Søknad lagt på kafka-topic $topic")
         } catch (kafkaException: KafkaException) {
             logger.warn("Innsending av melding feilet: ${kafkaException.message}")
             producer.abortTransaction()
-        } finally {
-            producer.flush()
-            producer.close()
         }
     }
 
