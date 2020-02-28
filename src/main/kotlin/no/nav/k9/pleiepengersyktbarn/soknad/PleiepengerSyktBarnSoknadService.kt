efb1704633ae21fcb2b576662d2848f590655be9
JKMNAV2020b <60650669+JKMNAV2020b@users.noreply.github.com>
2020-02-28 10:50:18 +0100

    Kafka functionality send (#67)
    * adding sendKafka message to send
    
    * La til dummy json melding.
    
    * La til log statements
    
    * Reduserte størrelse på tmp json streng
    
    * Ordnet properties i yaml fil
    Ordnet Exception håndtering
    
    * Refaktorering for å sende melding på Kafka-topic
    
    * Endrer generering av KafkaProducer samt innskyting av KafkaPropertyUtils
    
    * Endrer til mer Spring Boot-aktig implementasjon.
    
    * Fix MockConfig
    
    * Kaster exception ved feil
    
    * Fjerner dummyJson og tar i mot søknad
    
    Co-authored-by: Ola Løvholm <ola@lovholm.net>

--- src/main/kotlin/no/nav/k9/pleiepengersyktbarn/soknad/PleiepengerSyktBarnSoknadService.kt
+++ src/main/kotlin/no/nav/k9/pleiepengersyktbarn/soknad/PleiepengerSyktBarnSoknadService.kt
@@ -1,25 +1,28 @@
 package no.nav.k9.pleiepengersyktbarn.soknad
 
-import no.nav.k9.*
-import no.nav.k9.mappe.Mappe
+import no.nav.k9.kafka.HendelseProducer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+
 @Service
-internal class PleiepengerSyktBarnSoknadService {
+class PleiepengerSyktBarnSoknadService @Autowired constructor(
+        var hendelseProducer: HendelseProducer
+) {
 
     private companion object {
         private val logger: Logger = LoggerFactory.getLogger(PleiepengerSyktBarnSoknadService::class.java)
+        const val PLEIEPENGER_SYKT_BARN_TOPIC = "punsjet-soknad-pleiepenger-barn"
     }
 
     internal suspend fun sendSøknad(
-            norskIdent: NorskIdent,
-            mappe: Mappe
+            soeknadJson : String
     ) {
-        // TODO: Legge på en kafka-topic k9-fordel håndterer.
-        logger.info("sendSøknad")
-        logger.info("NorskIdent=$norskIdent")
-        logger.info("Mappe=$mappe")
+
+        hendelseProducer.sendTilKafkaTopic(PLEIEPENGER_SYKT_BARN_TOPIC, soeknadJson )
     }
+
+
 }
