92427bb26262cd28494eab87d396bed6b5ac3f15
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-10-14 06:39:45 +0200

    Fjerner hibernate

--- app/src/main/kotlin/no/nav/k9/pleiepengersyktbarn/soknad/PleiepengerSyktBarnEntity.kt
+++ app/src/main/kotlin/no/nav/k9/pleiepengersyktbarn/soknad/PleiepengerSyktBarnEntity.kt
@@ -12,28 +12,4 @@ data class PleiepengerSyktBarnEntity(
         val barn_norsk_ident: String?,
         val barn_fodselsdato: Date?,
         val soknad: String?
-)/* {
-    fun getIdMappe(): UUID {
-        return id_mappe;
-    }
-
-    fun getNorskIdent(): String {
-        return norsk_ident;
-    }
-
-    fun getSistEndret(): Timestamp {
-        return sist_endret;
-    }
-
-    fun getBarnNorskIdent(): String? {
-        return barn_norsk_ident;
-    }
-
-    fun getBarnFodselsdato(): Date? {
-        return barn_fodselsdato;
-    }
-
-    fun getSoknad(): String? {
-        return soknad;
-    }
-}*/
+)
