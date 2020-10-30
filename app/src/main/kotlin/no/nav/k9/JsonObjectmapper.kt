fc3a062668e28723889bcc49017722fcbd56ab00
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-10-30 13:23:46 +0100

    Tar bort avsenderMottaker fra log

--- app/src/main/kotlin/no/nav/k9/JsonObjectmapper.kt
+++ app/src/main/kotlin/no/nav/k9/JsonObjectmapper.kt
@@ -19,7 +19,6 @@ fun ObjectMapper.dusseldorfConfigured() : ObjectMapper {
     configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
-    configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
     propertyNamingStrategy = PropertyNamingStrategy.LOWER_CAMEL_CASE
     registerModule(JavaTimeModule())
     return this
