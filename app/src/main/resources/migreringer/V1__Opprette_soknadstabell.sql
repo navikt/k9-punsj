ab2230e1710227b5a3ea90fd9d6f07cb7901682c
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-11-10 09:44:21 +0100

    Gjør fila lik som master

--- app/src/main/resources/migreringer/V1__Opprette_soknadstabell.sql
+++ app/src/main/resources/migreringer/V1__Opprette_soknadstabell.sql
@@ -1,9 +1,9 @@
 create table if not exists soknader (
-                                      id_soknad           serial      primary key,
-                                      id_mappe            uuid        not null,
-                                      norsk_ident         char (11)   not null,
-                                      sist_endret         timestamp   not null,
-                                      barn_norsk_ident    char (11),
-                                      barn_fodselsdato    date,
-                                      soknad              json
+    id_soknad           serial      primary key,
+    id_mappe            uuid        not null,
+    norsk_ident         char (11)   not null,
+    sist_endret         timestamp   not null,
+    barn_norsk_ident    char (11),
+    barn_fodselsdato    date,
+    soknad              json
 )
