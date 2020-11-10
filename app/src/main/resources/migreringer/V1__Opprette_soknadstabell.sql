070f5c240be5432c2d80c8f064488a2b3ca76362
alexandraban <alexandrabandarenka@nav.no>
2020-11-10 08:27:04 +0100

    retter opp migreringer

--- app/src/main/resources/migreringer/V1__Opprette_soknadstabell.sql
+++ app/src/main/resources/migreringer/V1__Opprette_soknadstabell.sql
@@ -1,4 +1,9 @@
-create table if not exists journalpost (
-   JOURNALPOSTID   VARCHAR(100) NOT NULL PRIMARY KEY,
-   DATA jsonb        NOT NULL
+create table if not exists soknader (
+                                      id_soknad           serial      primary key,
+                                      id_mappe            uuid        not null,
+                                      norsk_ident         char (11)   not null,
+                                      sist_endret         timestamp   not null,
+                                      barn_norsk_ident    char (11),
+                                      barn_fodselsdato    date,
+                                      soknad              json
 )
