b5658c70e28a5d9a23e6bc14724eaca0462ad4e6
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-11-13 12:52:00 +0100

    Logger data ved sending til k9-los

--- app/src/main/kotlin/no/nav/k9/omsorgspenger/overfoerdager/OverførDagerSøknadService.kt
+++ app/src/main/kotlin/no/nav/k9/omsorgspenger/overfoerdager/OverførDagerSøknadService.kt
@@ -5,9 +5,10 @@ import no.nav.k9.fordel.PunsjEventDto
 import no.nav.k9.journalpost.JournalpostRepository
 import no.nav.k9.kafka.HendelseProducer
 import no.nav.k9.objectMapper
-import no.nav.k9.omsorgspenger.delingavomsorgsdager.DelingAvOmsorgsdagerMeldingService
 import no.nav.k9.rapid.behov.Behovssekvens
 import no.nav.k9.rapid.behov.OverføreOmsorgsdagerBehov
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -21,6 +22,7 @@ class OverførDagerSøknadService @Autowired constructor(
     private companion object {
         const val rapidTopic = "k9-rapid-v2"
         const val topicK9Los = "privat-k9punsj-aksjonspunkthendelse-v1"
+        private val log: Logger = LoggerFactory.getLogger(OverførDagerSøknadService::class.java)
     }
 
     internal suspend fun sendSøknad(søknad: OverføreOmsorgsdagerBehov, dedupKey: String) {
@@ -34,12 +36,14 @@ class OverførDagerSøknadService @Autowired constructor(
 
         for (jornalpostId in søknad.journalpostIder) {
             val jpost = journalpostRepository.hent(jornalpostId)
-            hendelseProducer.send(topicName = OverførDagerSøknadService.topicK9Los, data = objectMapper().writeValueAsString(PunsjEventDto(
+            val data = objectMapper().writeValueAsString(PunsjEventDto(
                     jpost.uuid.toString(),
                     jornalpostId,
                     jpost.aktørId,
                     LocalDateTime.now(),
-                    mutableListOf(Aksjonspunkt.AVSLUTTET))), key = id)
+                    mutableListOf(Aksjonspunkt.AVSLUTTET)))
+            log.info(data)
+            hendelseProducer.send(topicName = topicK9Los, data = data, key = id)
         }
     }
 }
