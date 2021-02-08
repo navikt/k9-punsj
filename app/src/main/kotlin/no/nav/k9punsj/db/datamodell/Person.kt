a2d54745d6fb6c22064bab3b8e4fbc872a0ef32a
Ole Jørgen Rishoff <olejri@gmail.com>
2021-02-08 22:54:16 +0100

    Trenger ikke kodeverk fra k9-sak

--- app/src/main/kotlin/no/nav/k9punsj/db/datamodell/Person.kt
+++ app/src/main/kotlin/no/nav/k9punsj/db/datamodell/Person.kt
@@ -8,24 +8,8 @@ data class Person(
     val personId: PersonId,
     val norskIdent: NorskIdent,
     val aktørId: AktørId
-) {
-    init {
-        no.nav.k9.sak.typer.PersonIdent(norskIdent)
-        no.nav.k9.sak.typer.AktørId(aktørId)
-    }
-}
+)
 
-internal fun Person.getNorskIdent(): NorskIdent {
-    return norskIdent
-}
-
-internal fun Person.getAktørId(): AktørId {
-    return aktørId
-}
-
-internal fun Person.getPersonId(): PersonId {
-    return personId;
-}
 
 
 
