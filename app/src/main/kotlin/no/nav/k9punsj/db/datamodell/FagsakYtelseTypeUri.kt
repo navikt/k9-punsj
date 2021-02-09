831380a6d12bad287bda938e8b8a6dbeab4695ae
Ole Jørgen Rishoff <olejri@gmail.com>
2021-02-09 11:00:03 +0100

    setter til riktig uri

--- app/src/main/kotlin/no/nav/k9punsj/db/datamodell/FagsakYtelseTypeUri.kt
+++ app/src/main/kotlin/no/nav/k9punsj/db/datamodell/FagsakYtelseTypeUri.kt
@@ -2,7 +2,7 @@ package no.nav.k9punsj.db.datamodell
 
 sealed class FagsakYtelseTypeUri {
         companion object {
-                internal const val PLEIEPENGER_SYKT_BARN = "pleiepenger-sykt-barn"
+                internal const val PLEIEPENGER_SYKT_BARN = "pleiepenger-sykt-barn-soknad"
                 internal const val OMSORGSPENGER_DELING = "omsorgspenger-deling-av-omsorgsdager-melding"
                 internal const val OMSORGSPENGER_OVERFØRING_DAGER = "omsorgspenger-overfoer-dager-soknad"
                 internal const val PLEIEPENGER_NÆRSTÅENDE = "ukjent-naerstaaende"
