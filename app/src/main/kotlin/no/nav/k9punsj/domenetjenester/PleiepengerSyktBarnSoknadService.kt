c5b467e83fb0c4f572324e56f9a9ddd677b3a316
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-05-10 20:01:49 +0200

    Fjerner dobbelinnsending.

--- app/src/main/kotlin/no/nav/k9punsj/domenetjenester/PleiepengerSyktBarnSoknadService.kt
+++ app/src/main/kotlin/no/nav/k9punsj/domenetjenester/PleiepengerSyktBarnSoknadService.kt
@@ -1,10 +1,5 @@
 package no.nav.k9punsj.domenetjenester
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.ArrayNode
-import com.fasterxml.jackson.databind.node.ObjectNode
-import kotlinx.coroutines.runBlocking
 import no.nav.k9.søknad.Søknad
 import no.nav.k9punsj.akjonspunkter.AksjonspunktKode
 import no.nav.k9punsj.akjonspunkter.AksjonspunktStatus
@@ -14,8 +9,6 @@ import no.nav.k9punsj.hentCorrelationId
 import no.nav.k9punsj.innsending.InnsendingClient
 import no.nav.k9punsj.journalpost.JournalpostRepository
 import no.nav.k9punsj.kafka.HendelseProducer
-import no.nav.k9punsj.kafka.Topics.Companion.PLEIEPENGER_SYKT_BARN_TOPIC
-import no.nav.k9punsj.objectMapper
 import no.nav.k9punsj.rest.web.JournalpostId
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -29,45 +22,25 @@ class PleiepengerSyktBarnSoknadService(
     val journalpostRepository: JournalpostRepository,
     val søknadRepository: SøknadRepository,
     val innsendingClient: InnsendingClient,
-    val aksjonspunktService: AksjonspunktService
-) {
-
-    private companion object {
-        private val logger: Logger = LoggerFactory.getLogger(PleiepengerSyktBarnSoknadService::class.java)
-    }
+    val aksjonspunktService: AksjonspunktService) {
 
     internal suspend fun sendSøknad(søknad: Søknad, journalpostIder: MutableSet<JournalpostId>) {
-        val dokumentfordelingMelding: String = toDokumentfordelingMelding(søknad, journalpostIder)
-
-        hendelseProducer.sendMedOnSuccess(
-            topicName = PLEIEPENGER_SYKT_BARN_TOPIC,
-            data = dokumentfordelingMelding,
-            key = søknad.søknadId.id
-        ) {
-           runBlocking {
-               journalpostRepository.settBehandletFerdig(journalpostIder)
-               søknadRepository.markerSomSendtInn(søknad.søknadId.id)
-               aksjonspunktService.settUtførtForAksjonspunkterOgSendLukkOppgaveTilK9Los(journalpostIder.toList(),
-                   Pair(AksjonspunktKode.PUNSJ, AksjonspunktStatus.UTFØRT))
-           }
-        }
-
-        // TODO: Fjern dobbel innsending
         innsendingClient.sendSøknad(
             søknadId = søknad.søknadId.id,
             søknad = søknad,
             correlationId = coroutineContext.hentCorrelationId()
         )
+        kotlin.runCatching { journalpostRepository.settBehandletFerdig(journalpostIder) }.onFailure { throwable ->
+            // TODO: Burde ikke fortsette når denne feilen oppstår.
+            logger.error("Feil ved setting av behandling ferdig for JournalpostIder=$journalpostIder", throwable)
+        }
+        søknadRepository.markerSomSendtInn(søknad.søknadId.id)
+        aksjonspunktService.settUtførtForAksjonspunkterOgSendLukkOppgaveTilK9Los(journalpostIder.toList(),
+            Pair(AksjonspunktKode.PUNSJ, AksjonspunktStatus.UTFØRT))
     }
 
-    fun toDokumentfordelingMelding(søknad: Any, journalpostIder: MutableSet<JournalpostId>): String {
-        // Midlertidig generering av meldings-JSON i påvente av et definert format.
-        val om: ObjectMapper = objectMapper()
-        val dokumentfordelingMelding: ObjectNode = om.createObjectNode()
-        val data: ObjectNode = dokumentfordelingMelding.objectNode()
-        data.set<JsonNode>("søknad", om.valueToTree(søknad))
-        data.set<ArrayNode>("journalpostIder", om.valueToTree<ArrayNode>(journalpostIder))
-        dokumentfordelingMelding.set<JsonNode>("data", data)
-        return dokumentfordelingMelding.toString()
+    private companion object {
+        private val logger: Logger = LoggerFactory.getLogger(PleiepengerSyktBarnSoknadService::class.java)
     }
+
 }
