0c8cd9d0729a6da5b4db1f191ac4a612f7e2bd3e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-09-22 10:51:26 +0200

    For lokal kjøring, er man født i Mai får man to perioder i K9

--- app/src/main/kotlin/no/nav/k9punsj/rest/eksternt/k9sak/K9SakServiceLokalt.kt
+++ app/src/main/kotlin/no/nav/k9punsj/rest/eksternt/k9sak/K9SakServiceLokalt.kt
@@ -5,6 +5,7 @@ import no.nav.k9punsj.db.datamodell.NorskIdent
 import no.nav.k9punsj.rest.web.dto.PeriodeDto
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import java.time.LocalDate
 
 @Configuration
 @Profile("local")
@@ -13,7 +14,17 @@ class K9SakServiceLokalt : K9SakService {
         søker: NorskIdent,
         barn: NorskIdent,
         fagsakYtelseType: FagsakYtelseType,
-    ): Pair<List<PeriodeDto>?, String?> {
-        return Pair(emptyList(), null)
+    ) = when (søker.erFødtIMai()) {
+        true -> Pair(listOf(
+            PeriodeDto(fom = LocalDate.now(), tom = LocalDate.now().plusWeeks(3)),
+            PeriodeDto(fom = LocalDate.now().minusMonths(6), tom = LocalDate.now().minusMonths(4))
+        ), null)
+        false -> Pair(emptyList(), null)
+    }
+
+    private companion object{
+        fun String.erFødtIMai() = kotlin.runCatching {
+            substring(2,4) == "05"
+        }.fold(onSuccess = { it }, onFailure = {false} )
     }
 }
