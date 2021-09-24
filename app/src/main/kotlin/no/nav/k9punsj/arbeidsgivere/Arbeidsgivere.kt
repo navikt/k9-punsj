a7b2045c875fed1691612a419229115ef0aa9f92
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-09-24 12:25:52 +0200

    Litt renaming

--- app/src/main/kotlin/no/nav/k9punsj/arbeidsgivere/Arbeidsgivere.kt
+++ app/src/main/kotlin/no/nav/k9punsj/arbeidsgivere/Arbeidsgivere.kt
@@ -1,10 +1,10 @@
 package no.nav.k9punsj.arbeidsgivere
 
+internal data class Arbeidsgivere(
+    val organisasjoner: Set<OrganisasjonArbeidsgiver>
+)
+
 internal data class OrganisasjonArbeidsgiver(
     val organisasjonsnummer: String,
     val navn: String
-)
-
-internal data class Arbeidsgivere(
-    val organisasjoner: Set<OrganisasjonArbeidsgiver>
 )
