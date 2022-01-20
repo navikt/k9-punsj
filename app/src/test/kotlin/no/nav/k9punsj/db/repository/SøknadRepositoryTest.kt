4d3400a2768c25a1bb6f5da79957fb4e43fbbe74
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-01-20 09:38:54 +0100

    Refaktorerer og forenkler test.

--- app/src/test/kotlin/no/nav/k9punsj/db/repository/SøknadRepositoryTest.kt
+++ app/src/test/kotlin/no/nav/k9punsj/db/repository/SøknadRepositoryTest.kt
@@ -39,47 +39,16 @@ internal class SøknadRepositoryTest {
     }
 
     @Test
-    fun `Skal lagre søknad`(): Unit = runBlocking {
-
-        val barnFødselsdato = LocalDate.now()
-
-        //oppretter en person
-        val person = personRepository.lagre(norskIdent = standardIdent, aktørId = standardAktørId)
-        val barn = personRepository.lagre(norskIdent = barnIdent, aktørId = barnAktørID)
-        assertThat(person.personId).isNotNull
-        assertThat(barn.personId).isNotNull
-
-        //oppretter en tom mappe til en person
-        val mappeId = mappeRepo.opprettEllerHentMappeForPerson(person.personId)
-
-        //oppretter en bunke i mappen for pleiepenger
-        val bunkeId = bunkeRepository.opprettEllerHentBunkeForFagsakType(mappeId, FagsakYtelseType.PLEIEPENGER_SYKT_BARN)
-
-        val genererKomplettSøknad = LesFraFilUtil.søknadFraFrontend()
-
-        val journalposter = mutableMapOf<String, Any?>()
-        journalposter["journalposter"] = listOf(journalpostid1, journalpostid2)
-
-        //oppretter en søknad og legger den i bunken
-        val søknadEntitet = SøknadEntitet(
-            søknadId = UUID.randomUUID().toString(),
-            bunkeId = bunkeId,
-            søkerId = person.personId,
-            barnId = barn.personId,
-            barnFødselsdato = barnFødselsdato,
-            søknad = genererKomplettSøknad,
-            journalposter = journalposter,
-            sendtInn = false
-        )
-
-        val søknad = søknadRepository.opprettSøknad(søknadEntitet)
-
-
-        assertThat(søknad.søknad).isNotNull
+    fun `Skal lagre pleiepenger sykt barn søknad`(): Unit = runBlocking {
+        opprettOgAssertSøknad(FagsakYtelseType.PLEIEPENGER_SYKT_BARN, LesFraFilUtil.søknadFraFrontend())
     }
 
     @Test
     fun `Skal lagre omsorgspenger kronisk sykt barn søknad`(): Unit = runBlocking {
+        opprettOgAssertSøknad(FagsakYtelseType.OMSORGSPENGER_KRONISK_SYKT_BARN, LesFraFilUtil.søknadUtenBarnFraFrontendOmsKSB())
+    }
+
+    private suspend fun opprettOgAssertSøknad(fagsakYtelseType: FagsakYtelseType, søknad: MutableMap<String, Any?>) {
         val barnFødselsdato = LocalDate.now()
 
         //oppretter en person
@@ -92,9 +61,7 @@ internal class SøknadRepositoryTest {
         val mappeId = mappeRepo.opprettEllerHentMappeForPerson(person.personId)
 
         //oppretter en bunke i mappen for pleiepenger
-        val bunkeId = bunkeRepository.opprettEllerHentBunkeForFagsakType(mappeId, FagsakYtelseType.OMSORGSPENGER_KRONISK_SYKT_BARN)
-
-        val genererKomplettSøknad = LesFraFilUtil.søknadFraFrontendOmsKSB()
+        val bunkeId = bunkeRepository.opprettEllerHentBunkeForFagsakType(mappeId, fagsakYtelseType)
 
         val journalposter = mutableMapOf<String, Any?>()
         journalposter["journalposter"] = listOf(journalpostid1, journalpostid2)
@@ -106,7 +73,7 @@ internal class SøknadRepositoryTest {
             søkerId = person.personId,
             barnId = barn.personId,
             barnFødselsdato = barnFødselsdato,
-            søknad = genererKomplettSøknad,
+            søknad = søknad,
             journalposter = journalposter,
             sendtInn = false
         )
