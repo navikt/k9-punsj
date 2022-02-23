cd1e548b5e2c82a23f9dd30df1798ce226b1a7d0
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-02-23 11:23:33 +0100

    Legger til fellesmetrikker på resten av ytelsene.

--- app/src/main/kotlin/no/nav/k9punsj/metrikker/SøknadMetrikker.kt
+++ app/src/main/kotlin/no/nav/k9punsj/metrikker/SøknadMetrikker.kt
@@ -4,9 +4,7 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
 import no.nav.k9.søknad.Søknad
 import no.nav.k9.søknad.ytelse.Ytelse
-import no.nav.k9.søknad.ytelse.pls.v1.PleipengerLivetsSluttfase
 import no.nav.k9.søknad.ytelse.psb.v1.PleiepengerSyktBarn
-import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.Arbeidstid
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.temporal.ChronoUnit
@@ -17,20 +15,41 @@ private val logger = LoggerFactory.getLogger("no.nav.k9punsj.metrikker.SøknadMe
 class SøknadMetrikkService(
     private val meterRegistry: MeterRegistry
 ) {
+
     fun publiserMetrikker(søknad: Søknad) {
-        logger.info("Publiserer søknadsmetrikker.")
+        try {
+            val type: Ytelse.Type = søknad.getYtelse<Ytelse>().type
+            when (type) {
+                Ytelse.Type.PLEIEPENGER_SYKT_BARN -> psbMetrikker(søknad)
+                Ytelse.Type.OMSORGSPENGER_UTVIDETRETT_KRONISK_SYKT_BARN -> fellesMetrikker(søknad)
+                Ytelse.Type.OMSORGSPENGER_UTVIDETRETT_MIDLERTIDIG_ALENE -> fellesMetrikker(søknad)
+                Ytelse.Type.OMSORGSPENGER_UTVIDETRETT_ALENE_OMSORG -> fellesMetrikker(søknad)
+                Ytelse.Type.OMSORGSPENGER_UTBETALING -> fellesMetrikker(søknad)
+                Ytelse.Type.PLEIEPENGER_LIVETS_SLUTTFASE -> fellesMetrikker(søknad)
+            }
+        } catch (e: Exception) {
+            logger.warn("Feilet med publisering av metrikker", e)
+        }
+    }
+
+    fun fellesMetrikker(søknad: Søknad) {
         val ytelse = søknad.getYtelse<Ytelse>()
-        val søknadstype = hentType(ytelse)
+        val søknadstype = ytelse.type.name
         val søknadsId = søknad.søknadId.id
         val defaultTags = mutableListOf(
             Tag.of("soknadsId", søknadsId),
             Tag.of("soknadstype", søknadstype)
         )
         meterRegistry.Config().commonTags(defaultTags)
-
         meterRegistry.counter(ANTALL_INNSENDINGER).increment()
+    }
 
-        hentSøknadsperiode(ytelse)?.apply {
+    private fun psbMetrikker(søknad: Søknad) {
+        logger.info("Publiserer søknadsmetrikker.")
+        val ytelse = søknad.getYtelse<PleiepengerSyktBarn>()
+        fellesMetrikker(søknad)
+
+        hentSøknadsperiodeUker(ytelse)?.apply {
             meterRegistry.summary(ANTALL_UKER_SØKNADER_GJELDER_BUCKET).record(this)
         }
 
@@ -48,63 +67,43 @@ class SøknadMetrikkService(
             ).increment()
         }
 
-        hentArbeidstid(ytelse)?.apply {
-            meterRegistry.summary(ANTALL_ARBEIDSGIVERE_BUCKET, defaultTags)
-                .record(this.arbeidstakerList.size.toDouble())
+        ytelse.arbeidstid.apply {
+            if (this.arbeidstakerList.isNotEmpty()) {
+                meterRegistry.summary(ANTALL_ARBEIDSGIVERE_BUCKET)
+                    .record(this.arbeidstakerList.size.toDouble())
+            }
 
             this.frilanserArbeidstidInfo.ifPresent {
-                meterRegistry.counter(ARBEIDSTID_FRILANSER_COUNTER, defaultTags).increment()
+                meterRegistry.counter(ARBEIDSTID_FRILANSER_COUNTER).increment()
             }
 
             this.selvstendigNæringsdrivendeArbeidstidInfo.ifPresent {
-                meterRegistry.counter(ARBEIDSTID_SELVSTENDING_COUNTER, defaultTags).increment()
+                meterRegistry.counter(ARBEIDSTID_SELVSTENDING_COUNTER).increment()
             }
         }
 
-        hentBeredskap(ytelse)?.apply {
-            meterRegistry.counter(BEREDSKAP_COUNTER, defaultTags).increment()
+        ytelse.beredskap.apply {
+            if (this.perioder.isNotEmpty()) {
+                meterRegistry.counter(BEREDSKAP_COUNTER).increment()
+            }
         }
 
-        hentNatteVåk(ytelse)?.apply {
-            meterRegistry.counter(NATTEVAAK_COUNTER, defaultTags).increment()
+        ytelse.nattevåk.apply {
+            if (this.perioder.isNotEmpty()) {
+                meterRegistry.counter(NATTEVAAK_COUNTER).increment()
+            }
         }
 
-        hentTilsynsordning(ytelse)?.apply {
-            meterRegistry.counter(TILSYNSORDNING_COUNTER, defaultTags).increment()
-        }
-    }
-
-    fun hentType(ytelse: Ytelse): String {
-        return ytelse.type.name
-    }
-
-    fun hentArbeidstid(ytelse: Ytelse): Arbeidstid? {
-        return when (ytelse) {
-            is PleiepengerSyktBarn -> ytelse.arbeidstid
-            is PleipengerLivetsSluttfase -> ytelse.arbeidstid
-            else -> null
+        ytelse.tilsynsordning.apply {
+            if (this.perioder.isNotEmpty()) {
+                meterRegistry.counter(TILSYNSORDNING_COUNTER).increment()
+            }
         }
     }
 
-    fun hentSøknadsperiode(ytelse: Ytelse): Double? {
+    fun hentSøknadsperiodeUker(ytelse: Ytelse): Double? {
         val søknadsperiode = runCatching { ytelse.søknadsperiode }.getOrNull() ?: return null
-        //legger på en dag siden until ikke tar med siste
         return ChronoUnit.WEEKS.between(søknadsperiode.fraOgMed, søknadsperiode.tilOgMed).toDouble()
-    }
-
-    fun hentBeredskap(ytelse: Ytelse) = when (ytelse) {
-        is PleiepengerSyktBarn -> ytelse.beredskap
-        else -> null
-    }
-
-    fun hentNatteVåk(ytelse: Ytelse) = when (ytelse) {
-        is PleiepengerSyktBarn -> ytelse.nattevåk
-        else -> null
-    }
-
-    fun hentTilsynsordning(ytelse: Ytelse) = when (ytelse) {
-        is PleiepengerSyktBarn -> ytelse.tilsynsordning
-        else -> null
     }
 
     companion object {
--- app/src/test/kotlin/no/nav/k9punsj/metrikker/SøknadMetrikkServiceTest.kt
+++ app/src/test/kotlin/no/nav/k9punsj/metrikker/SøknadMetrikkServiceTest.kt
@@ -3,7 +3,8 @@ package no.nav.k9punsj.metrikker
 import io.micrometer.core.instrument.Statistic
 import io.micrometer.core.instrument.Tag
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
-import no.nav.k9punsj.domenetjenester.mappers.MapPsbTilK9Format
+import no.nav.k9.søknad.ytelse.Ytelse.Type
+import no.nav.k9punsj.domenetjenester.mappers.*
 import no.nav.k9punsj.metrikker.SøknadMetrikkService.Companion.ANTALL_ARBEIDSGIVERE_BUCKET
 import no.nav.k9punsj.metrikker.SøknadMetrikkService.Companion.ANTALL_INNSENDINGER
 import no.nav.k9punsj.metrikker.SøknadMetrikkService.Companion.ANTALL_UKER_SØKNADER_GJELDER_BUCKET
@@ -15,7 +16,7 @@ import no.nav.k9punsj.metrikker.SøknadMetrikkService.Companion.NATTEVAAK_COUNTE
 import no.nav.k9punsj.metrikker.SøknadMetrikkService.Companion.TILSYNSORDNING_COUNTER
 import no.nav.k9punsj.objectMapper
 import no.nav.k9punsj.rest.web.SøknadJson
-import no.nav.k9punsj.rest.web.dto.PleiepengerSyktBarnSøknadDto
+import no.nav.k9punsj.rest.web.dto.*
 import no.nav.k9punsj.util.LesFraFilUtil
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -37,13 +38,13 @@ internal class SøknadMetrikkServiceTest {
     }
 
     @Test
-    internal fun forvent_riktig_publiserte_metrikker() {
+    internal fun forvent_riktig_publiserte_psb_metrikker() {
         val gyldigSoeknad: SøknadJson = LesFraFilUtil.søknadFraFrontend()
         val dto = objectMapper().convertValue(gyldigSoeknad, PleiepengerSyktBarnSøknadDto::class.java)
         val k9Format = MapPsbTilK9Format(dto.soeknadId, setOf("123", "456"), emptyList(), dto).søknad()
         søknadMetrikkService.publiserMetrikker(k9Format)
 
-        val søknadstypeTag = Tag.of("soknadstype", "PLEIEPENGER_SYKT_BARN")
+        val søknadstypeTag = Tag.of("soknadstype", Type.PLEIEPENGER_SYKT_BARN.name)
         val søknadsIdTag = Tag.of("soknadsId", dto.soeknadId)
 
         assertCounter(metric = ANTALL_INNSENDINGER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
@@ -53,7 +54,9 @@ internal class SøknadMetrikkServiceTest {
         assertCounter(metric = BEREDSKAP_COUNTER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
         assertCounter(metric = NATTEVAAK_COUNTER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
         assertCounter(metric = TILSYNSORDNING_COUNTER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
-        assertBucket(metric = ANTALL_UKER_SØKNADER_GJELDER_BUCKET, forventetVerdi = 42.0, søknadstypeTag, søknadsIdTag)
+
+        val forventetAntallUker = 42.0
+        assertBucket(metric = ANTALL_UKER_SØKNADER_GJELDER_BUCKET, forventetVerdi = forventetAntallUker, søknadstypeTag, søknadsIdTag)
         assertBucket(metric = ANTALL_ARBEIDSGIVERE_BUCKET, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
 
         assertCounter(
@@ -64,6 +67,71 @@ internal class SøknadMetrikkServiceTest {
             Tag.of("antall_journalposter", "2"),
             Tag.of("opplysninger", "IkkeKanPunsjes=true | MedOpplysninger=false")
         )
+    }
+
+    @Test
+    internal fun forvent_riktig_publiserte_oms_ao_metrikker() {
+        val gyldigSoeknad: SøknadJson = LesFraFilUtil.søknadFraFrontendOmsAO()
+        val dto = objectMapper().convertValue(gyldigSoeknad, OmsorgspengerAleneOmsorgSøknadDto::class.java)
+        val k9Format = MapOmsAOTilK9Format(dto.soeknadId, setOf("123", "456"), dto).søknad()
+        søknadMetrikkService.publiserMetrikker(k9Format)
+
+        val søknadstypeTag = Tag.of("soknadstype", Type.OMSORGSPENGER_UTVIDETRETT_ALENE_OMSORG.name)
+        val søknadsIdTag = Tag.of("soknadsId", dto.soeknadId)
+
+        assertCounter(metric = ANTALL_INNSENDINGER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
+    }
+
+    @Test
+    internal fun forvent_riktig_publiserte_oms_ma_metrikker() {
+        val gyldigSoeknad: SøknadJson = LesFraFilUtil.søknadFraFrontendOmsMA()
+        val dto = objectMapper().convertValue(gyldigSoeknad, OmsorgspengerMidlertidigAleneSøknadDto::class.java)
+        val k9Format = MapOmsMATilK9Format(dto.soeknadId, setOf("123", "456"), dto).søknad()
+        søknadMetrikkService.publiserMetrikker(k9Format)
+
+        val søknadstypeTag = Tag.of("soknadstype", Type.OMSORGSPENGER_UTVIDETRETT_MIDLERTIDIG_ALENE.name)
+        val søknadsIdTag = Tag.of("soknadsId", dto.soeknadId)
+
+        assertCounter(metric = ANTALL_INNSENDINGER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
+    }
+
+    @Test
+    internal fun forvent_riktig_publiserte_pls_metrikker() {
+        val gyldigSoeknad: SøknadJson = LesFraFilUtil.søknadFraFrontendPls()
+        val dto = objectMapper().convertValue(gyldigSoeknad, PleiepengerLivetsSluttfaseSøknadDto::class.java)
+        val k9Format = MapPlsfTilK9Format(dto.soeknadId, setOf("123", "456"), dto).søknad()
+        søknadMetrikkService.publiserMetrikker(k9Format)
+
+            val søknadstypeTag = Tag.of("soknadstype", Type.PLEIEPENGER_LIVETS_SLUTTFASE.name)
+        val søknadsIdTag = Tag.of("soknadsId", dto.soeknadId)
+
+        assertCounter(metric = ANTALL_INNSENDINGER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
+    }
+
+    @Test
+    internal fun forvent_riktig_publiserte_oms_metrikker() {
+        val gyldigSoeknad: SøknadJson = LesFraFilUtil.søknadFraFrontendOmsKSB()
+        val dto = objectMapper().convertValue(gyldigSoeknad, OmsorgspengerSøknadDto::class.java)
+        val k9Format = MapOmsTilK9Format(dto.soeknadId, setOf("123", "456"), dto).søknad()
+        søknadMetrikkService.publiserMetrikker(k9Format)
+
+        val søknadstypeTag = Tag.of("soknadstype", Type.OMSORGSPENGER_UTBETALING.name)
+        val søknadsIdTag = Tag.of("soknadsId", dto.soeknadId)
+
+        assertCounter(metric = ANTALL_INNSENDINGER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
+    }
+
+    @Test
+    internal fun forvent_riktig_publiserte_omp_ks_metrikker() {
+        val gyldigSoeknad: SøknadJson = LesFraFilUtil.søknadFraFrontendOmsKSB()
+        val dto = objectMapper().convertValue(gyldigSoeknad, OmsorgspengerKroniskSyktBarnSøknadDto::class.java)
+        val k9Format = MapOmsKSBTilK9Format(dto.soeknadId, setOf("123", "456"), dto).søknad()
+        søknadMetrikkService.publiserMetrikker(k9Format)
+
+        val søknadstypeTag = Tag.of("soknadstype", Type.OMSORGSPENGER_UTVIDETRETT_KRONISK_SYKT_BARN.name)
+        val søknadsIdTag = Tag.of("soknadsId", dto.soeknadId)
+
+        assertCounter(metric = ANTALL_INNSENDINGER, forventetVerdi = 1.0, søknadstypeTag, søknadsIdTag)
     }
 
     private fun assertCounter(metric: String, forventetVerdi: Double, vararg tags: Tag) {
