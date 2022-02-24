eed46a75d8a9751e49444352d31125c96abefcfb
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-02-24 10:56:07 +0100

    Logger kjøring av metrikkjobb.

--- app/src/main/kotlin/no/nav/k9punsj/jobber/MetrikkJobb.kt
+++ app/src/main/kotlin/no/nav/k9punsj/jobber/MetrikkJobb.kt
@@ -4,17 +4,23 @@ import io.micrometer.core.instrument.MeterRegistry
 import kotlinx.coroutines.runBlocking
 import no.nav.k9punsj.journalpost.JournalpostMetrikkRepository
 import no.nav.k9punsj.metrikker.Metrikk
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class MetrikkJobb(
     val journalpostMetrikkRepo: JournalpostMetrikkRepository,
     val meterRegistry: MeterRegistry
 ) {
+    private companion object {
+        private val logger = LoggerFactory.getLogger(MetrikkJobb::class.java)
+    }
 
     @Scheduled(cron = "0 0 3 * * *")
     fun oppdaterMetrikkMåling() = runBlocking {
+        logger.info("Kjører metrikk jobb: {}", LocalDateTime.now())
         val antallFerdigBehandledeJournalposter = journalpostMetrikkRepo.hentAntallFerdigBehandledeJournalposter(true)
         meterRegistry.gauge(Metrikk.ANTALL_FERDIG_BEHANDLEDE_JOURNALPOSTER.navn, antallFerdigBehandledeJournalposter)
 
