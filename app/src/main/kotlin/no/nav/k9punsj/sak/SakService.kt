69c5584cc47f70aedfcca18611c81a0509ece75a
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-03-14 09:38:48 +0100

    Debuggervmapping av jsonObject

--- app/src/main/kotlin/no/nav/k9punsj/sak/SakService.kt
+++ app/src/main/kotlin/no/nav/k9punsj/sak/SakService.kt
@@ -3,6 +3,7 @@ package no.nav.k9punsj.sak
 import no.nav.k9punsj.journalpost.SafGateway
 import org.json.JSONArray
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,12 +11,20 @@ class SakService(
     private val safGateway: SafGateway
 ) {
 
+    private companion object {
+        private val logger = LoggerFactory.getLogger(SakService::class.java)
+    }
+
     suspend fun hentSaker(søkerIdent: String): List<SakInfo> {
         val sakerFraSaf: JSONArray? = safGateway.hentSakerFraSaf(søkerIdent)
         return when {
             sakerFraSaf != null -> {
                 sakerFraSaf
-                    .mapIndexed { index: Int, _ -> sakerFraSaf.getJSONObject(index).somSakInfo() }
+                    .mapIndexed { index: Int, it ->
+                        val jsonObject = sakerFraSaf.getJSONObject(index)
+                        logger.info("Mapper jsonObject til sakInfo: {}", jsonObject.toString())
+                        jsonObject.somSakInfo()
+                    }
                     .filter { "OMS" === it.tema }
             }
             else -> listOf()
