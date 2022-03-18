1effa6de561d5c25e2e34ec8449a2c957073e1fe
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-03-18 14:04:00 +0100

    Refaktorerer oppslag av fagsaker. (#508)
    * Refaktorer oppslag av fagsaker.
    
    Henter fagsaker med fagsakId og sakstype fra k9-sak.
    
    * Henter kode fra sakstype og mapper til fagsakYtelseType navn.

--- app/src/test/kotlin/no/nav/k9punsj/rest/eksternt/k9sak/LokalK9SakService.kt
+++ app/src/test/kotlin/no/nav/k9punsj/rest/eksternt/k9sak/LokalK9SakService.kt
@@ -30,4 +30,13 @@ class LokalK9SakService : K9SakService {
         fagsakYtelseType: FagsakYtelseType,
         periodeDto: PeriodeDto,
     ) : Pair<List<ArbeidsgiverMedArbeidsforholdId>?, String?> = Pair(emptyList(), null)
+
+    override suspend fun hentFagsaker(søker: String): Pair<Set<Fagsak>?, String?> = Pair(
+        first = setOf(
+            Fagsak(saksnummer = "ABC123", no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_SYKT_BARN),
+            Fagsak(saksnummer = "DEF456", no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_NÆRSTÅENDE),
+            Fagsak(saksnummer = "GHI789", no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER_KS)
+        ),
+        second = null
+    )
 }
