67446d2d7cbc4a213d5cfa701af6e79609b9c500
albrektsson <emil.albrektsson@nav.no>
2022-03-22 09:30:55 +0100

    Loggfilter-fiks

--- app/src/main/kotlin/no/nav/k9punsj/LoggFilter.kt
+++ app/src/main/kotlin/no/nav/k9punsj/LoggFilter.kt
@@ -5,13 +5,17 @@ import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
-import java.util.UUID
+import java.util.*
 
 @Component
 class LoggFilter :WebFilter {
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         val callIdKey = exchange.request.headers["x_callId"]?.firstOrNull() ?: UUID.randomUUID().toString()
-        exchange.request.headers.add("punsj_callId", callIdKey)
+        val response = exchange.response
+        response.beforeCommit {
+            response.headers.set("punsj_callId", callIdKey)
+            return@beforeCommit Mono.empty()
+        }
         return chain.filter(exchange)
     }
 }
