cbf234bae3615b6f15d5796e13cb827de581ee89
albrektsson <emil.albrektsson@nav.no>
2022-03-24 13:43:42 +0100

    Brevkoden snek sig ur saf queryn

--- app/src/main/kotlin/no/nav/k9punsj/dokarkiv/SafDtos.kt
+++ app/src/main/kotlin/no/nav/k9punsj/dokarkiv/SafDtos.kt
@@ -22,6 +22,7 @@ internal object SafDtos {
                 }
                 dokumenter {
                   dokumentInfoId
+                  brevkode
                   dokumentvarianter {
                     variantformat
                     saksbehandlerHarTilgang
