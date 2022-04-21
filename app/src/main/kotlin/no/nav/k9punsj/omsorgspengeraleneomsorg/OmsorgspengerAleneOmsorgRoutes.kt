c80cb44cfb6d31e5a798a3c544d7a57b0e523b1a
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-04-21 15:49:56 +0200

    Fikser pathVariabel i HentSøknad url.

--- app/src/main/kotlin/no/nav/k9punsj/omsorgspengeraleneomsorg/OmsorgspengerAleneOmsorgRoutes.kt
+++ app/src/main/kotlin/no/nav/k9punsj/omsorgspengeraleneomsorg/OmsorgspengerAleneOmsorgRoutes.kt
@@ -31,7 +31,7 @@ internal class OmsorgspengerAleneOmsorgRoutes(
 
     internal object Urls {
         internal const val HenteMappe = "/$søknadType/mappe" //get
-        internal const val HenteSøknad = "/$søknadType/mappe/$søknadIdKey" //get
+        internal const val HenteSøknad = "/$søknadType/mappe/{$søknadIdKey}" //get
         internal const val NySøknad = "/$søknadType" //post
         internal const val OppdaterEksisterendeSøknad = "/$søknadType/oppdater" //put
         internal const val SendEksisterendeSøknad = "/$søknadType/send" //post
