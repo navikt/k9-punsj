03c1c89c96f4ea71968ee76495e69a1a67ce9d84
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-08-26 09:35:04 +0200

    Legger til swagger definisjon for å lukke oppgave.

--- app/src/main/kotlin/no/nav/k9punsj/integrasjoner/gosys/GosysOpenApi.kt
+++ app/src/main/kotlin/no/nav/k9punsj/integrasjoner/gosys/GosysOpenApi.kt
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import no.nav.k9punsj.openapi.OasFeil
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -107,7 +108,7 @@ internal class GosysOpenApi {
         description = "Lukker gosysoppgave",
         security = [SecurityRequirement(name = "BearerAuth")]
     )
-    fun lukkGosysoppgave() {
+    fun lukkGosysoppgave(@PathVariable gosysoppgaveId: String) {
     }
 
     @GetMapping(GosysRoutes.Urls.Gjelder, produces = ["application/json"])
