de59eba10d3a29e649c3cc5f68f48ce966ebb2b4
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-09-16 08:54:16 +0200

    Retter typo

--- app/src/test/kotlin/no/nav/k9punsj/felles/SakTest.kt
+++ app/src/test/kotlin/no/nav/k9punsj/felles/SakTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class SakTest {
 
     @Test
-    fun `gitt at sakstype er fagsag, forvent at fagsaksystem er K9`() {
+    fun `gitt at sakstype er fagsak, forvent at fagsaksystem er K9`() {
         val sak = Sak(sakstype = Sak.SaksType.FAGSAK, fagsakId = "ABC123")
         assertThat(sak.fagsaksystem).isNotNull.isEqualTo(Sak.FagsakSystem.K9)
     }
