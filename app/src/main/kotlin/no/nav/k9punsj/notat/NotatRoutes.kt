7c688434a4bf5fa648a20434b0b17712b71a6c69
albrektsson <emil.albrektsson@nav.no>
2022-12-06 08:42:34 +0100

    Azure token for k9sak, fjernet notat toggle

--- app/src/main/kotlin/no/nav/k9punsj/notat/NotatRoutes.kt
+++ app/src/main/kotlin/no/nav/k9punsj/notat/NotatRoutes.kt
@@ -7,7 +7,6 @@ import no.nav.k9punsj.openapi.OasFeil
 import no.nav.k9punsj.tilgangskontroll.AuthenticationHandler
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
@@ -21,8 +20,7 @@ import kotlin.coroutines.coroutineContext
 @Configuration
 internal class NotatRoutes(
     private val authenticationHandler: AuthenticationHandler,
-    private val notatService: NotatService,
-    @Value("\${NOTAT_ENABLED}") private val notatEnabled: Boolean
+    private val notatService: NotatService
 ) {
 
     internal companion object {
@@ -39,36 +37,29 @@ internal class NotatRoutes(
     @Bean
     fun NotatRoutes() = SaksbehandlerRoutes(authenticationHandler) {
         POST("/api${Urls.OpprettNotat}") { request ->
-            if (notatEnabled) {
-                RequestContext(coroutineContext, request) {
-                    val nyNotat = request.nyNotat()
+            RequestContext(coroutineContext, request) {
+                val nyNotat = request.nyNotat()
 
-                    return@RequestContext kotlin.runCatching {
-                        logger.info("Oppretter notat...")
-                        notatService.opprettNotat(nyNotat)
-                    }
-                }.fold(
-                    onSuccess = {
-                        logger.info("Notat opprettet.")
-                        ServerResponse
-                            .status(HttpStatus.CREATED)
-                            .json()
-                            .bodyValueAndAwait(it)
-                    },
-                    onFailure = {
-                        logger.error("Feilet med å opprette notat.", it)
-                        ServerResponse
-                            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                            .json()
-                            .bodyValueAndAwait(OasFeil(it.message))
-                    }
-                )
-            } else {
-                ServerResponse
-                    .status(HttpStatus.NOT_IMPLEMENTED)
-                    .json()
-                    .bodyValueAndAwait("Ikke enablet")
-            }
+                return@RequestContext kotlin.runCatching {
+                    logger.info("Oppretter notat...")
+                    notatService.opprettNotat(nyNotat)
+                }
+            }.fold(
+                onSuccess = {
+                    logger.info("Notat opprettet.")
+                    ServerResponse
+                        .status(HttpStatus.CREATED)
+                        .json()
+                        .bodyValueAndAwait(it)
+                },
+                onFailure = {
+                    logger.error("Feilet med å opprette notat.", it)
+                    ServerResponse
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json()
+                        .bodyValueAndAwait(OasFeil(it.message))
+                }
+            )
         }
     }
 }
