c27504e9d079da40bce323ea2dedccc6db83cd42
albrektsson <emil.albrektsson@nav.no>
2023-01-10 12:49:26 +0100

    Json-test

--- app/src/test/kotlin/no/nav/k9punsj/opplaeringspenger/MapOlpTilK9FormatTest.kt
+++ app/src/test/kotlin/no/nav/k9punsj/opplaeringspenger/MapOlpTilK9FormatTest.kt
@@ -2,6 +2,7 @@ package no.nav.k9punsj.opplaeringspenger
 
 import no.nav.k9.søknad.felles.Feil
 import no.nav.k9punsj.felles.dto.PeriodeDto
+import no.nav.k9punsj.objectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -48,6 +49,60 @@ internal class MapOlpTilK9FormatTest {
         assert(søknadsperiode != null)
         assert(søknadsperiode!!.fom == LocalDate.of(2023, 1, 1))
         assert(søknadsperiode!!.tom == LocalDate.of(2023, 1, 29))
+    }
+
+    @Test
+    fun `test json-payload mapper o validerer`() {
+        val json = """
+            {
+              "arbeidstid": {
+                "arbeidstakerList": [],
+                "frilanserArbeidstidInfo": null,
+                "selvstendigNæringsdrivendeArbeidstidInfo": null
+              },
+              "barn": { "norskIdent": "03091477490", "foedselsdato": "" },
+              "begrunnelseForInnsending": { "tekst": "" },
+              "bosteder": [],
+              "harInfoSomIkkeKanPunsjes": false,
+              "harMedisinskeOpplysninger": true,
+              "journalposter": ["200"],
+              "klokkeslett": "12:53",
+              "kurs": {
+                "kursHolder": {
+                  "institusjonsUuid": "8671b60e-fd47-4097-b005-c376ea0fa240"
+                },
+                "kursperioder": [
+                  {
+                    "periode": { "fom": "2023-01-01", "tom": "2023-01-31" },
+                    "avreise": "2023-01-01",
+                    "hjemkomst": "2023-01-31"
+                  }
+                ]
+              },
+              "lovbestemtFerie": [],
+              "mottattDato": "2020-10-12",
+              "omsorg": {
+                "relasjonTilBarnet": "",
+                "beskrivelseAvOmsorgsrollen": "",
+                "samtykketOmsorgForBarnet": false
+              },
+              "opptjeningAktivitet": {
+                "arbeidstaker": [],
+                "selvstendigNaeringsdrivende": null,
+                "frilanser": null
+              },
+              "soekerId": "29099000129",
+              "soeknadId": "c9f9d5aa-535d-4da3-af8b-76e245ed1794",
+              "soeknadsperiode": null,
+              "soknadsinfo": { "samtidigHjemme": null, "harMedsøker": null },
+              "trekkKravPerioder": [],
+              "utenlandsopphold": [],
+              "uttak": []
+            }
+        """.trimIndent()
+
+        val dto = objectMapper().readValue(json, OpplaeringspengerSøknadDto::class.java)
+        assert(dto.feil().isEmpty())
     }
 
     private fun KursPeriode(fom: LocalDate, tom: LocalDate) = OpplaeringspengerSøknadDto.KursPeriodeMedReisetid(
