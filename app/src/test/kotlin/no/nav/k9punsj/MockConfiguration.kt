4e233e71ed1c110af115ad0887875d3c4c05f120
albrektsson <emil.albrektsson@nav.no>
2023-02-22 11:27:34 +0100

    Riktig scope for k9-infotrygd

--- app/src/test/kotlin/no/nav/k9punsj/MockConfiguration.kt
+++ app/src/test/kotlin/no/nav/k9punsj/MockConfiguration.kt
@@ -66,7 +66,6 @@ internal object MockConfiguration {
             "EREG_BASE_URL" to wireMockServer.getEregBaseUrl(),
             "SEND_BREVBESTILLING_TIL_K9_FORMIDLING" to "privat-k9-dokumenthendelse",
             "SEND_AKSJONSPUNKTHENDELSE_TIL_K9LOS" to "privat-k9punsj-aksjonspunkthendelse-v1",
-            "NOTAT_ENABLED" to "true",
             "K9_INFOTRYGD_BASE_URL" to wireMockServer.getInfotrygdGrunnlagPaaroerendeSykdomBaseUrl(),
             "K9_INFOTRYGD_SCOPE" to "infotrygd/.default",
         )
