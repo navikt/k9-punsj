cfd9b6ff2adb3949e4e471ec04afd8af79a894a3
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2023-10-29 11:39:28 +0100

    Abac-kall går via inloggetUtils, ryddet opp i pepclient, erstattet gson med jackson (#1024)

--- src/main/kotlin/no/nav/k9punsj/pleiepengerlivetssluttfase/PleiepengerLivetsSluttfaseRoutes.kt
+++ src/main/kotlin/no/nav/k9punsj/pleiepengerlivetssluttfase/PleiepengerLivetsSluttfaseRoutes.kt
@@ -45,7 +45,7 @@ internal class PleiepengerLivetsSluttfaseRoutes(
         GET("/api${Urls.HenteMappe}") { request ->
             RequestContext(coroutineContext, request) {
                 val norskIdent = request.hentNorskIdentHeader()
-                innlogget.harInnloggetBrukerTilgangTilOgSendeInn(norskIdent = norskIdent, url = Urls.HenteMappe)
+                innlogget.harInnloggetBrukerTilgangTilOgSkriveSakForFnr(fnr = norskIdent, url = Urls.HenteMappe)
                     ?.let { return@RequestContext it }
 
                 pleiepengerLivetsSluttfaseService.henteMappe(norskIdent)
@@ -69,8 +69,8 @@ internal class PleiepengerLivetsSluttfaseRoutes(
         POST("/api${Urls.SendEksisterendeSøknad}") { request ->
             RequestContext(coroutineContext, request) {
                 val sendSøknad = request.mapSendSøknad()
-                innlogget.harInnloggetBrukerTilgangTilOgSendeInn(
-                    norskIdent = sendSøknad.norskIdent,
+                innlogget.harInnloggetBrukerTilgangTilOgSkriveSakForFnr(
+                    fnr = sendSøknad.norskIdent,
                     url = Urls.SendEksisterendeSøknad
                 )?.let { return@RequestContext it }
 
@@ -81,8 +81,8 @@ internal class PleiepengerLivetsSluttfaseRoutes(
         POST("/api${Urls.NySøknad}", contentType(MediaType.APPLICATION_JSON)) { request ->
             RequestContext(coroutineContext, request) {
                 val opprettNySøknad = request.mapNySøknad()
-                innlogget.harInnloggetBrukerTilgangTilOgSendeInn(
-                    norskIdent = opprettNySøknad.norskIdent,
+                innlogget.harInnloggetBrukerTilgangTilOgSkriveSakForFnr(
+                    fnr = opprettNySøknad.norskIdent,
                     url = Urls.NySøknad
                 )?.let { return@RequestContext it }
 
@@ -94,8 +94,8 @@ internal class PleiepengerLivetsSluttfaseRoutes(
             RequestContext(coroutineContext, request) {
                 val soknadTilValidering = request.pleiepengerPlsSøknad()
                 soknadTilValidering.soekerId?.let { norskIdent ->
-                    innlogget.harInnloggetBrukerTilgangTilOgSendeInn(
-                        norskIdent = norskIdent,
+                    innlogget.harInnloggetBrukerTilgangTilOgSkriveSakForFnr(
+                        fnr = norskIdent,
                         url = Urls.ValiderSøknad
                     )?.let { return@RequestContext it }
                 }
@@ -107,8 +107,8 @@ internal class PleiepengerLivetsSluttfaseRoutes(
         POST("/api${Urls.HentInfoFraK9sak}") { request ->
             RequestContext(coroutineContext, request) {
                 val matchfagsak = request.mapMatchFagsak()
-                innlogget.harInnloggetBrukerTilgangTil(
-                    norskIdentDto = listOf(matchfagsak.brukerIdent, matchfagsak.barnIdent!!),
+                innlogget.harInnloggetBrukerTilgangTilOgSkriveSakForFnr(
+                    fnrList = listOf(matchfagsak.brukerIdent, matchfagsak.barnIdent!!),
                     url = Urls.HentInfoFraK9sak
                 )?.let { return@RequestContext it }
 
