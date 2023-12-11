f5ae33a8b2e73b98fb2122a81abc67d8902ccced
albrektsson <emil.albrektsson@nav.no>
2023-12-11 11:38:19 +0100

    Deprecatat copy pasta dto fra punsjbollen

--- src/main/kotlin/no/nav/k9punsj/integrasjoner/k9sak/HentK9SaksnummerGrunnlagDto.kt
+++ src/main/kotlin/no/nav/k9punsj/integrasjoner/k9sak/HentK9SaksnummerGrunnlagDto.kt
@@ -3,6 +3,7 @@ package no.nav.k9punsj.integrasjoner.k9sak
 import no.nav.k9punsj.felles.FagsakYtelseType
 import no.nav.k9punsj.felles.dto.PeriodeDto
 
+@Deprecated("Erstattes med bedre løsning v.h.a. k9-format?")
 data class HentK9SaksnummerGrunnlag(
     val søknadstype: FagsakYtelseType,
     val søker: String,
