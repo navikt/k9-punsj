0a35ce46a4301d024124be2b3dfc24d7a392d137
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-12-20 08:58:18 +0100

    Fikser automatisk flyway migrering.

--- src/main/kotlin/no/nav/k9punsj/K9PunsjApplication.kt
+++ src/main/kotlin/no/nav/k9punsj/K9PunsjApplication.kt
@@ -7,7 +7,6 @@ import de.huxhorn.sulky.ulid.ULID
 import no.nav.k9punsj.utils.UlidDeserializer
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory
@@ -15,10 +14,9 @@ import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.scheduling.annotation.EnableScheduling
-import javax.sql.DataSource
 
 @EnableScheduling
-@SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class, FlywayAutoConfiguration::class])
+@SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
 class K9PunsjApplication {
 
     @Bean
