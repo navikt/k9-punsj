c350e744451a5293205156270d6f58d8f86c120d
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-01-05 11:38:11 +0100

    Bruker AbstractContainerBaseTest for MappeRepositoryTest.

--- src/test/kotlin/no/nav/k9punsj/pleiepengersyktbarn/MappeRepositoryTest.kt
+++ src/test/kotlin/no/nav/k9punsj/pleiepengersyktbarn/MappeRepositoryTest.kt
@@ -1,27 +1,29 @@
 package no.nav.k9punsj.pleiepengersyktbarn
 
 import kotlinx.coroutines.runBlocking
-import no.nav.k9punsj.util.DbContainerInitializer
+import no.nav.k9punsj.AbstractContainerBaseTest
+import no.nav.k9punsj.domenetjenester.repository.MappeRepository
+import no.nav.k9punsj.domenetjenester.repository.PersonRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.test.context.junit.jupiter.SpringExtension
-/*
-@ExtendWith(SpringExtension::class)
-internal class MappeRepositoryTest {
+import org.springframework.beans.factory.annotation.Autowired
+
+internal class MappeRepositoryTest: AbstractContainerBaseTest() {
+
+    @Autowired
+    lateinit var mappeRepository: MappeRepository
+
+    @Autowired
+    lateinit var personRepository: PersonRepository
 
     private val dummyFnr = "11111111111"
     private val dummyAktørId = "1000000000000"
 
     @Test
     internal fun HentAlleMapperSomInneholderEnNorskIdent(): Unit = runBlocking {
-        val repository = DbContainerInitializer.getMappeRepo()
-        val personRepo = DbContainerInitializer.getPersonRepo()
+        val person = personRepository.lagre(norskIdent = dummyFnr, aktørId = dummyAktørId)
 
-        val person = personRepo.lagre(norskIdent = dummyFnr, aktørId = dummyAktørId)
-
-        val mappe = repository.opprettEllerHentMappeForPerson(personId = person.personId)
+        val mappe = mappeRepository.opprettEllerHentMappeForPerson(personId = person.personId)
         assertThat(mappe).isNotNull
     }
 }
-*/
