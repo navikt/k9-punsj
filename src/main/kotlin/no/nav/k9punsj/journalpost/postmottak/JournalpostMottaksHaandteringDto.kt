2957d4b432995cc989dbf845ecb614c12d4cb0b7
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-02-05 09:04:39 +0100

    Fjerner ubrukte felter.

--- src/main/kotlin/no/nav/k9punsj/journalpost/postmottak/JournalpostMottaksHaandteringDto.kt
+++ src/main/kotlin/no/nav/k9punsj/journalpost/postmottak/JournalpostMottaksHaandteringDto.kt
@@ -1,13 +1,8 @@
 package no.nav.k9punsj.journalpost.postmottak
 
-import no.nav.k9punsj.felles.dto.PeriodeDto
-
 data class JournalpostMottaksHaandteringDto(
     val journalpostId: String,
     val brukerIdent: String,
     val fagsakYtelseTypeKode: String,
-    val saksnummer: String?,
-    val barnIdent: String?, // TODO: Trenges ikke?
-    val annenPart: String?,// TODO: Trenges ikke?
-    val periode: PeriodeDto?,// TODO: Trenges ikke?
+    val saksnummer: String? // Settes kun ved tilknytning mot eksisterende sak.
 )
