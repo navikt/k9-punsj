808aafcc95ea559d0d3ffe9ebfe880601090c79e
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-02-23 10:28:56 +0100

    Legger på problem-detail validering for postmottak.

--- src/main/kotlin/no/nav/k9punsj/journalpost/postmottak/JournalpostMottaksHaandteringDto.kt
+++ src/main/kotlin/no/nav/k9punsj/journalpost/postmottak/JournalpostMottaksHaandteringDto.kt
@@ -1,9 +1,35 @@
 package no.nav.k9punsj.journalpost.postmottak
 
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER_AO
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER_KS
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER_MA
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.OPPLÆRINGSPENGER
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_NÆRSTÅENDE
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_SYKT_BARN
+import org.springframework.http.HttpStatus
+
 data class JournalpostMottaksHaandteringDto(
     val journalpostId: String,
     val brukerIdent: String,
     val barnIdent: String?, // Settes kun ved tilknytning mot reservert saksnummer.
     val fagsakYtelseTypeKode: String,
-    val saksnummer: String? // Settes kun ved tilknytning mot eksisterende sak.
-)
+    val saksnummer: String?, // Settes kun ved tilknytning mot eksisterende sak.
+    val behandlingsÅr: Int? = null // Skal settes hvis fagsakYtelseTypeKode gjelder omsorgspenger
+) {
+    init {
+        val ytelseType = FagsakYtelseType.fromString(fagsakYtelseTypeKode)
+        when(ytelseType) {
+            PLEIEPENGER_SYKT_BARN, PLEIEPENGER_NÆRSTÅENDE -> {} // OK
+            OMSORGSPENGER, OMSORGSPENGER_KS, OMSORGSPENGER_MA, OMSORGSPENGER_AO -> {
+                if (behandlingsÅr == null || (behandlingsÅr !in 2000..2100)) {
+                    throw PostMottakException("Ugyldig verdi behandlingsÅr ($behandlingsÅr) for ytelseType $fagsakYtelseTypeKode", HttpStatus.BAD_REQUEST, journalpostId)
+                }
+
+            }
+            OPPLÆRINGSPENGER -> TODO()
+            else -> throw IllegalArgumentException("Ikke støttet ytelseType: $fagsakYtelseTypeKode")
+        }
+    }
+}
