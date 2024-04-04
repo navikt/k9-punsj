435d8509c0db41880b7918638ea3ef5a41893ca6
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-04-04 12:09:22 +0200

    Håndterer k9sakPeriode.tom der det er null.

--- src/main/kotlin/no/nav/k9punsj/integrasjoner/k9sak/K9SakServiceImpl.kt
+++ src/main/kotlin/no/nav/k9punsj/integrasjoner/k9sak/K9SakServiceImpl.kt
@@ -455,7 +455,7 @@ class K9SakServiceImpl(
                 "journalpostId": "$journalpostId",
                 "periode": {
                     "fom": "${k9sakPeriode.fom}",
-                    "tom": "${k9sakPeriode.tom}"
+                    "tom": ${k9sakPeriode.tom?.let { "$it" } ?: null}
                 },
                 "ytelseType": {
                     "kode": "${fagsakYtelseType.kode}",
