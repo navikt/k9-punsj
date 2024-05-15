e574ccd47f603441d7461613b8faf15ca2a04c69
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-05-15 15:09:11 +0200

    Erstatter Controller med @RouterOperation

--- src/main/kotlin/no/nav/k9punsj/forvaltning/ForvaltningRoutes.kt
+++ src/main/kotlin/no/nav/k9punsj/forvaltning/ForvaltningRoutes.kt
@@ -1,5 +1,7 @@
 package no.nav.k9punsj.forvaltning
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import no.nav.k9punsj.RequestContext
 import no.nav.k9punsj.SaksbehandlerRoutes
 import no.nav.k9punsj.domenetjenester.MappeService
@@ -14,7 +16,7 @@ import kotlin.coroutines.coroutineContext
 @Configuration
 internal class ForvaltningRoutes(
     private val authenticationHandler: AuthenticationHandler,
-    private val mappeService: MappeService
+    private val mappeService: MappeService,
 ) {
 
     internal object Urls {
@@ -23,12 +25,21 @@ internal class ForvaltningRoutes(
     }
 
     @Bean
-    @RouterOperation(beanClass = MappeService::class, beanMethod = "slettMappeMedAlleKoblinger")
+    @RouterOperation(
+        operation = Operation(
+            operationId = "Sletter alle mapper med tilknyttede søknader og bunker",
+            summary = "Sletter alle mapper med tilknyttede søknader og bunker",
+            tags = ["Forvaltning"],
+            responses = [
+                ApiResponse(responseCode = "200", description = "Alle mapper med bunker og søknader er slettet"),
+                ApiResponse(responseCode = "500", description = "Noe gikk galt")
+            ]
+        )
+    )
     fun ForvaltningRoutes() = SaksbehandlerRoutes(authenticationHandler) {
         POST("/api${Urls.SlettMappeMedAlleTilkoblinger}") { request ->
             RequestContext(coroutineContext, request) {
                 mappeService.slettMappeMedAlleKoblinger()
-
                 return@RequestContext ServerResponse.ok().buildAndAwait()
             }
         }
