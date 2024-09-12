679fc1435b298efd1e3382811a64ea056b89a2bf
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2024-09-12 09:01:16 +0200

    Testdata og mock journalpost for OLP (#1233)
    * Legg til mock journalpost for OLP
    
    * Konsistent uuid for institusjoner i test

--- src/test/kotlin/no/nav/k9punsj/rest/eksternt/k9sak/LokalK9SakService.kt
+++ src/test/kotlin/no/nav/k9punsj/rest/eksternt/k9sak/LokalK9SakService.kt
@@ -152,26 +152,103 @@ class LokalK9SakService : K9SakService {
 
     override suspend fun hentInstitusjoner(): List<GodkjentOpplæringsinstitusjonDto> {
         val gyldigePerioder = listOf(Periode(LocalDate.now().minusMonths(12), LocalDate.now()))
+
         return listOf(
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehus Asker/Bærum", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Buskerud (Drammen sykehus)", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset i Vestfold", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehus Asker/Bærum", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Innlandet Elverum", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Innlandet Gjøvik", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Innlandet Hamar", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Innlandet Kongsvinger", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Innlandet Lillehammer", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Innlandet Tynset", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Telemark", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sykehuset Østfold", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sørlandet sykehus, Arendal", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sørlandet sykehus, Farsund", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sørlandet sykehus, Kristiansand", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sørlandet sykehus, Mandal", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "St. Olavs Hospital", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Stavanger Universitetssykehus", gyldigePerioder),
-            GodkjentOpplæringsinstitusjonDto(UUID.randomUUID(), "Sunnaas sykehus", gyldigePerioder),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("dccf90ba-5b3f-475b-9026-af0fa771d6c1"),
+                "Sykehus Asker/Bærum",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("ce14ae31-b365-48e6-becb-efa5450dbd7c"),
+                "Sykehuset Buskerud (Drammen sykehus)",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("7b2f8fac-8863-4794-b97d-aa35bc8e43f1"),
+                "Sykehuset i Vestfold",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("202c3f51-fc70-4ecb-8bf1-2eb20daf6346"),
+                "Sykehus Asker/Bærum",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("7a53ef39-4cac-4866-a8ae-3784af75fc91"),
+                "Sykehuset Innlandet Elverum",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("731adb3d-cfed-4ae1-a972-2f270236cc78"),
+                "Sykehuset Innlandet Gjøvik",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("df816f00-7407-45e9-a6fb-abb4c4cd5b9a"),
+                "Sykehuset Innlandet Hamar",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("455d2fb6-68e6-48c0-9100-9a30b17228a5"),
+                "Sykehuset Innlandet Kongsvinger",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("2562d2f5-4038-47c7-a007-e720b83abf50"),
+                "Sykehuset Innlandet Lillehammer",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("1cbbc81e-4193-4dfa-a622-d01cbb577203"),
+                "Sykehuset Innlandet Tynset",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("87ca816a-0361-42d5-ad34-a0cff1446fb0"),
+                "Sykehuset Telemark",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("1661b54c-10f8-4d01-9e94-487551d9f0f9"),
+                "Sykehuset Østfold",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("3a4d57d6-4022-4e8c-9236-d3f6f366e13b"),
+                "Sørlandet sykehus, Arendal",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("8f29f0ea-3d59-4a39-9e2e-e81b7f562eaf"),
+                "Sørlandet sykehus, Farsund",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("617f81fb-6405-456c-a270-6620dcbf624c"),
+                "Sørlandet sykehus, Kristiansand",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("fe402564-c764-4acc-b23d-6e6a05389ff2"),
+                "Sørlandet sykehus, Mandal",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("1879b639-3505-4437-a936-0d4b7521f314"),
+                "St. Olavs Hospital",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("66c45d21-885b-4d8d-9a44-7d26fd8450b3"),
+                "Stavanger Universitetssykehus",
+                gyldigePerioder
+            ),
+            GodkjentOpplæringsinstitusjonDto(
+                UUID.fromString("b85d7f35-4b71-49a0-9522-7c30d85ea747"),
+                "Sunnaas sykehus",
+                gyldigePerioder
+            ),
         )
     }
 }
