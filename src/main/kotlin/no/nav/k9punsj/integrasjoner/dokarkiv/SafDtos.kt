23d592e43b2d7c713d7e31e796eb19a655898078
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-09-12 12:25:28 +0200

    Etterspørr datoOpprettet ved kall mot SAF.

--- src/main/kotlin/no/nav/k9punsj/integrasjoner/dokarkiv/SafDtos.kt
+++ src/main/kotlin/no/nav/k9punsj/integrasjoner/dokarkiv/SafDtos.kt
@@ -14,6 +14,7 @@ internal object SafDtos {
                 tema
                 tittel
                 journalposttype
+                datoOpprettet
                 relevanteDatoer {
                   dato
                   datotype
