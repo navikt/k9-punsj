266bf5786fd3a5139201fe3bc0d6ca49baab7aa4
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2025-12-06 17:40:45 +0100

    TSFF-2346: OLP - Tillate reservering av nytt saksnummer selv om det finnes en sak i k9

--- src/main/kotlin/no/nav/k9punsj/journalpost/postmottak/PostMottakService.kt
+++ src/main/kotlin/no/nav/k9punsj/journalpost/postmottak/PostMottakService.kt
@@ -56,24 +56,26 @@ class PostMottakService(
         val oppdatertJournalpost = hentOgOppdaterJournalpostFraDB(mottattJournalpost)
         val validertMottattJournalpost = mottattJournalpost.valider(oppdatertJournalpost.behandlingsAar)
 
-        logger.info("Verifiserer at det ikke finnes eksisterende fagsak for pleietrengende når man reserverer saksnummer.")
         val eksisterendeSaksnummer = mottattJournalpost.saksnummer
 
-        k9SakService.hentFagsaker(brukerIdent).first
-            ?.let { fagsaker ->
-                fagsaker.firstOrNull {
-                    it.pleietrengendeAktorId == pleietrengendeAktørId
-                            && it.sakstype == fagsakYtelseType
-                            && (it.gyldigPeriode?.fom?.year == oppdatertJournalpost.behandlingsAar || oppdatertJournalpost.behandlingsAar == null)
-                }
-                    ?.takeIf { eksisterendeSaksnummer == null }?.let { eksisterendeFagsak ->
-                        throw PostMottakException(
-                            melding = "Det eksisterer allerede en fagsak(${eksisterendeFagsak.sakstype.name} - ${eksisterendeFagsak.saksnummer}) på pleietrengende.",
-                            httpStatus = HttpStatus.CONFLICT,
-                            journalpostId = mottattJournalpost.journalpostId
-                        )
+        if (fagsakYtelseType != FagsakYtelseType.OPPLÆRINGSPENGER) {
+            // Verifiserer at det ikke finnes eksisterende fagsak for pleietrengende når man reserverer saksnummer.
+            k9SakService.hentFagsaker(brukerIdent).first
+                ?.let { fagsaker ->
+                    fagsaker.firstOrNull {
+                        it.pleietrengendeAktorId == pleietrengendeAktørId
+                                && it.sakstype == fagsakYtelseType
+                                && (it.gyldigPeriode?.fom?.year == oppdatertJournalpost.behandlingsAar || oppdatertJournalpost.behandlingsAar == null)
                     }
-            }
+                        ?.takeIf { eksisterendeSaksnummer == null }?.let { eksisterendeFagsak ->
+                            throw PostMottakException(
+                                melding = "Det eksisterer allerede en fagsak(${eksisterendeFagsak.sakstype.name} - ${eksisterendeFagsak.saksnummer}) på pleietrengende.",
+                                httpStatus = HttpStatus.CONFLICT,
+                                journalpostId = mottattJournalpost.journalpostId
+                            )
+                        }
+                }
+        }
 
         val safJournalpostinfo = hentJournalpostInfoFraSaf(oppdatertJournalpost)
 
