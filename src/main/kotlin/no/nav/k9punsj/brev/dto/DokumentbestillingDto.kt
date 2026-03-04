2d6df516ec69a457d61cabfe0e2d0c7f97dd7807
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-03-04 16:32:56 +0100

    fikser null saksnummer

--- src/main/kotlin/no/nav/k9punsj/brev/dto/DokumentbestillingDto.kt
+++ src/main/kotlin/no/nav/k9punsj/brev/dto/DokumentbestillingDto.kt
@@ -1,11 +1,14 @@
 package no.nav.k9punsj.brev.dto
 
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
 import no.nav.k9.formidling.kontrakt.kodeverk.FagsakYtelseType
 import java.util.*
 
 data class DokumentbestillingDto(
     val journalpostId: String?,
     val brevId: String? = UUID.randomUUID().toString(),
+    @param:JsonSetter(nulls = Nulls.SKIP)
     val saksnummer: String = "GENERELL_SAK",
     val soekerId: String,
     val mottaker: MottakerDto,
